a2dfe01e47d75d70cdf17946db18247c7a4d2f98
hermesonbastos <hermesonbastos@gmail.com>
2024-08-23 13:34:45 -0300

    01: all about routing

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Main Page';
+});
+
+// redirecting from an old route to a new route
+Route::get('/hallo', function () {
+    return redirect()->route('Hello');
+});
+
+// route with name
+Route::get('/hello', function () {
+    return "Hello!";
+})->name("Hello");
+
+// route with parameter
+Route::get('/greet/{name}', function ($name) {
+    return "Hello " . $name . "!";
+});
+
+// generic path to a non-existent route
+Route::fallback(function () {
+    return "Rota inexistente!";
 });
